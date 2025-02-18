81c2294d457816d0ee3ee7fae7f5ad3d926b301e
MarcosReyes <marcosreyesdesigner@gmail.com>
2025-02-18 16:34:42 -0400

    arreglo altura header

--- src/data/countryData.js
+++ src/data/countryData.js
@@ -7,6 +7,11 @@ const countryData = {
         { imagen: "https://logos-world.net/wp-content/uploads/2021/09/Orange-Emblem.png", nombre: "Orange" },
         { imagen: "https://edeeste.com.do/transparencia/wp-content/uploads/2017/01/Logo-EDEEste-Azul-Amarillo.png", nombre: "EdeEste" },
         { imagen: "https://edenorte.com.do/wp-content/uploads/2016/04/LOGOS-EDE.png", nombre: "Edenorte" },
+        { imagen: "https://upload.wikimedia.org/wikipedia/commons/f/fd/Logo_de_Edesur_Dominicana%2C_S.A.png", nombre: "Edesur" },
+        { imagen: "https://upload.wikimedia.org/wikipedia/commons/e/e7/Altice_logo_%28new%29.png", nombre: "Altice" },
+        { imagen: "https://logos-world.net/wp-content/uploads/2021/09/Orange-Emblem.png", nombre: "Orange" },
+        { imagen: "https://edeeste.com.do/transparencia/wp-content/uploads/2017/01/Logo-EDEEste-Azul-Amarillo.png", nombre: "EdeEste" },
+        { imagen: "https://edenorte.com.do/wp-content/uploads/2016/04/LOGOS-EDE.png", nombre: "Edenorte" },
         { imagen: "https://upload.wikimedia.org/wikipedia/commons/f/fd/Logo_de_Edesur_Dominicana%2C_S.A.png", nombre: "Edesur" }
       ]
     },
